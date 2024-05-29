3465930ad54aeea95806adf553ead5ec237f1246
Eduard-Gergil <nifigasegamer@gmail.com>
2024-05-29 12:05:40 +0500

    Initial commit

---
+++ components/nav-section.php
@@ -0,0 +1,25 @@
+<section class="section nav-section">
+  <a class="logo logo__link">
+    <img class="logo__img" src="assets/icons/logo.svg" alt="" />
+  </a>
+
+  <nav class="main-nav">
+    <ul class="main-nav__list">
+      <li class="main-nav__item main-nav__item--active">
+        <a class="main-nav__link" href="#">
+          <img class="main-nav__icon main-nav__icon--hidden" src="assets/icons/home.svg" alt="" />
+          <img class="main-nav__icon" src="assets/icons/home--active.svg" alt="" />
+          <span class="main-nav__name">Home</span>
+        </a>
+      </li>
+
+      <li class="main-nav__item">
+        <a class="main-nav__link" href="#">
+          <img class="main-nav__icon" src="assets/icons/search.svg" alt="" />
+          <img class="main-nav__icon main-nav__icon--hidden" src="assets/icons/search--active.svg" alt="" />
+          <span class="main-nav__name">Search</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</section>
---
+++ components/your-library-section/your-library-section.php
@@ -0,0 +1,16 @@
+<section class="section my-library-section">
+  <div class="my-library-section__header">
+    <div class="my-library-section__header-left">
+      <img src="/assets/icons/library.svg" alt="" class="my-library-section__icon">
+      <div class="my-library-section__title">Your Library</div>
+    </div>
+
+    <button class="my-library-section__plus-button">
+      <img src="/assets/icons/plus.svg" alt="" class="my-library-section__plus-icon">
+    </button>
+  </div>
+
+  <div class="my-library-section__body">
+    <?php include_once "./components/your-library-section/call-to-action/call-to-action.php"; ?>
+  </div>
+</section>
